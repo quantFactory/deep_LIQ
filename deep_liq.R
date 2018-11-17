740e7aa990c13ed3ce9ddbf30c1e197d442b1a14
Ryan <ghxalireza@gmail.com>
2018-11-17 13:38:41 +0800

    fixed department variable

--- deep_liq.R
+++ deep_liq.R
@@ -3,7 +3,8 @@ install.packages('dplyr')
 library(dplyr)
 
 attrition_df<-  read.csv("F://deepLearning/ibm-hr-analytics-employee-attrition-performance/Attrition.csv")
-
+write.csv(attrition_df,file = "F://deepLearning/attrition_df_r.csv")
+glimpse(attrition_df)
 
 
 #attrition_df
@@ -11,9 +12,9 @@ attrition_df<-  read.csv("F://deepLearning/ibm-hr-analytics-employee-attrition-p
 attrition_df <- attrition_df %>% mutate(Attrition_ = ifelse(Attrition =="Yes",1,0)) ; attrition_df
 
 #removing the original attrition_dfa column that contains Yes,No values
-attrition_df[["attrition_df"]] <- NULL
+attrition_df[["Attrition"]] <- NULL
 #renaming attrition_df_ to attrition_df
-names(attrition_df)[35] <- "attrition_df"
+names(attrition_df)[35] <- "Attrition"
 
 
 #Bussiness Travel
@@ -22,11 +23,33 @@ attrition_df <- attrition_df %>% mutate(BusinessTravel_ = case_when(
                                                     BusinessTravel == "Travel_Rarely" ~3,
                                                     BusinessTravel == "Non-Travel" ~ 1))
 
+attrition_df[["BusinessTravel"]] <- NULL
+
+
+names(attrition_df)[34] <- "Attrition"
+str(attrition_df)
+names(attrition_df)[35] <- "BusinessTravel"
+
+##department
+attrition_df <-  attrition_df %>% mutate(Department_ = case_when(
+                                                        Department == "Human Resources" ~ 1,
+                                                        Department == "Research & Development" ~ 2,
+                                                        Department == "Sales" ~ 3))
+
+
+str(attrition_df)
+
+attrition_df$Department
 
 
 
 
 
-write.csv(attrition_df,file = "F://deepLearning/attrition_df_r.csv")
+
+
+
+
+
+
 
 
