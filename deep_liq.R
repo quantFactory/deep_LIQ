3be59ca181039cbb3cd922e879b2607a3fd6c811
Ryan <ghxalireza@gmail.com>
2018-11-17 13:02:59 +0800

    fixing attrition

--- deep_liq.R
+++ deep_liq.R
@@ -2,16 +2,13 @@
 install.packages('dplyr')
 library(dplyr)
 
-attrition_df_df <-  read.csv("F:/deepLearning/ibm-hr-analytics-employee-attrition_df-performance/attrition_df.csv")
+attrition_df<-  read.csv("F://deepLearning/ibm-hr-analytics-employee-attrition-performance/Attrition.csv")
 
 
-toYesNO  <- function(dataframe){
-  
-  
-}
+
 #attrition_df
 #mutate new coluimn attrition_df_ to hold 0 and 1 for attrition_df and the ndeleting it 
-attrition_df_df <- attrition_df %>% mutate(attrition_df_ = ifelse(attrition_df =="Yes",1,0)) ; attrition_df
+attrition_df <- attrition_df %>% mutate(Attrition_ = ifelse(Attrition =="Yes",1,0)) ; attrition_df
 
 #removing the original attrition_dfa column that contains Yes,No values
 attrition_df[["attrition_df"]] <- NULL
