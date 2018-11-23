23db9290840db2ccf4460ac71091ca01782cf5bb
quant <developer.quant@gmail.com>
2018-11-24 00:31:34 +0800

    return p as json
    return attrition probability as json

---
+++ flask_apps/predict_app.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 21 19:48:03 2018
+
+@author: ali
+"""
+import keras
+from keras.models import load_model
+import h5py
+from keras.models import print_function
+from flask import Flask, request
+import tensorflow as tf
+import flask
+import numpy as np 
+from numpy import array
+from keras.backend import clear_session
+import pandas as pd 
+from flask import jsonify
+import json
+#loading model 
+model_ = load_model("./atr_subset.h5")
+graph = tf.get_default_graph()     
+model_.summary()
+
+x=1
+Age = 30 
+MartialStatus = 1 
+Gender =2 
+
+#testing model prediction 
+Xnew = array([[48,1,1]])
+Attrition_predict = model_.predict(Xnew)
+Attrition_predict
+
+input_ = pd.read_csv("../input.csv")
+pr = model_.predict_proba(input_)
+pr
+type(pr)
+pr_list = pr.tolist()
+type(pr_list)
+json_result = json.dumps(pr_list)
+json_result
+type(json_result)
+
+    
+#running flask app 
+app = flask.Flask(__name__)
+@app.route("/", methods=['POST'])
+def index():
+    global graph
+    with graph.as_default():
+        csv = pd.read_csv(request.files.get("input"))
+        proba = model_.predict_proba(csv)
+        proba_as_list = proba.tolist()
+        json_proba = json.dumps(proba_as_list) 
+        return (json_proba)
+  
+
+@app.route("/predict", methods=['GET','POST'])    
+def predict():
+    global graph
+    with graph.as_default():        
+        input_data = pd.read_csv(request.files.get("input_file"))        
+        predicted_probability = model_.predict_proba(input_data)
+        
+        return str(predicted_probability)
+     
+if __name__  ==  "__main__":
+    app.run()
